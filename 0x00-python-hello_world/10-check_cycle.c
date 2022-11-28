13c2715e8cbb93cec5e6ed3fd3789ae389e257ff
Samuel <samueldressman20@gmail.com>
2022-11-28 09:55:11 -0800

    My first python program

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,20 +1,37 @@
 #include "lists.h"
 /**
- * check_cycle - check a singly linked list has a cycle in it
- *
- * @list: linked list
- * Return: 0 or 1
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *nextn = list, *nextn_nextn = list;
+	listint_t *p2;
+	listint_t *prev;
 
-	while (nextn && nextn_nextn  && nextn_nextn->next)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		nextn = nextn->next;
-		nextn_nextn  = nextn_nextn->next->next;
-		if (nextn == nextn_nextn)
+		list = list->next;
+		p2 = p2->next->next;
+		if (list == p2)
 		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+				list = list->next;
+			}
 			return (1);
 		}
 	}
